ae0f81272d8e5e9c7a22c7a966779a645c198e61
Umar Saleem <umarsaleem0816@gmail.com>
2024-11-19 23:10:54 +0500

    Add files via upload

---
+++ Practice  01/TASK 03.py
@@ -0,0 +1,4 @@
+print('1')
+print('23')
+print('456')
+print('7891')
---
+++ Practice  01/TASK 04.py
@@ -0,0 +1,5 @@
+print('NAME\tSEMESTER\tGPA')
+print('ALI\tSECOND \t3.50')
+print('AHMED\tTHIRD \t3.33')
+print('RIZWAN\tFOURTH \t3.87')
+print('SADIA\tFIFTH \t4.00')
